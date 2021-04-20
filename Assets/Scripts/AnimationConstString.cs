fb07f1df01be579d422790207f9e6fb1da43b371
IAMFTSH <58794854+IAMFTSH@users.noreply.github.com>
2021-04-20 10:26:46 +0800

    首次提交

---
+++ Assets/Scripts/AnimationConstString.cs
@@ -0,0 +1,9 @@
+
+public class AnimationConstString
+{
+    public const string HURTING = "Hurting";
+    public const string RUNNING = "Running";
+    public const string JUMPING = "Jumping";
+    public const string FALLING = "Falling";
+
+}
---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class GameData
+{
+    private int cherry;
+    public int Cherry
+    {
+        get { return cherry; }
+        set { cherry = value; }
+    }
+
+}
